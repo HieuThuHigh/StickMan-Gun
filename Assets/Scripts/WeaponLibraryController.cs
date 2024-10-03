b843e35c6795069057405c24377e6267a1d5b5cf
HieuThuHigh <lehieu2526@gmail.com>
2024-10-03 17:07:50 +0700

    WeaponEvent

---
+++ Assets/Scripts/WeaponLibraryController.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponLibraryController : MonoBehaviour
+{
+   [SerializeField] private Button[] buttons;
+   [SerializeField] private GameObject[] gameObjectsPopup;
+   
+   private Color _defaultColor = Color.white;
+   private Color _selectedColor  = Color.yellow;
+
+   private void Start()
+   {
+      for (int i = 0; i < buttons.Length; i++)
+      {
+         int index = i;
+         buttons[i].onClick.AddListener(() => EventClicked(index));
+      }
+      ResetButtonColors();
+   }
+
+   void ResetButtonColors()
+   {
+      for (int i = 0; i < buttons.Length; i++)
+      {
+         buttons[i].image.color = _defaultColor;
+      }
+   }
+
+   void EventClicked(int index)
+   {
+      ResetButtonColors();
+      for (int i = 0; i < gameObjectsPopup.Length; i++)
+      {
+         gameObjectsPopup[i].SetActive(i == index);
+      }
+      buttons[index].image.color = _selectedColor;
+   }
+
+}
