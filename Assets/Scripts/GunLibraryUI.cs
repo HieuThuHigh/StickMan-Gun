5b81f0d7f2b99c589c14a43c1cd2eba578e37ae6
HieuThuHigh <lehieu2526@gmail.com>
2024-10-09 09:36:23 +0700

    Commit Comlplain

---
+++ Assets/Scripts/GunLibraryUI.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DefaultNamespace
+{
+    public class GunLibraryUI : MonoBehaviour
+    {
+        [SerializeField] private Image img;
+        [SerializeField] private WeaponInfo _weaponInfo;
+        [SerializeField] private Button _button;
+
+        public WeaponInfo WeaponInfo
+        {
+            get => _weaponInfo;
+            set => _weaponInfo = value;
+        }
+
+        private void Awake()
+        {
+            _button.onClick.AddListener(() =>
+            {
+                
+            });
+        }
+    }
+}
